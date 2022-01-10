9af335030e199faf589473da19d8499194a06970
吴淑超 <anyukj@qq.com>
2022-01-10 14:28:23 +0800

    初始化提交

--- vue.config.js
+++ vue.config.js
@@ -15,8 +15,7 @@ module.exports = {
     proxy: {
       '/api': {
         // 代理地址，地址为/api/开始的会转向target
-        // target: 'http://47.92.204.125:8096',
-        target: 'http://47.119.113.51:8010/',
+        target: 'http://chao.ac.cn:8010/',
         // 如果接口跨域，需要进行这个参数配置
         changeOrigin: true,
         pathRewrite: { '^/api': '/' },
